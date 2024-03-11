d5e6e1cb09dedba81db5d736ae9821770ec3d080
Guilherme Almeida <160321302+Guialmeidasx@users.noreply.github.com>
2024-03-11 20:39:06 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿    Console.WriteLine ("Todos os dias quando acordo");
+    Console.WriteLine ("Não tenho mais");
+    Console.WriteLine ("O tempo que passou");
+    Console.WriteLine ("Mas tenho muito tempo");
+    Console.WriteLine ("Temos todo o tempo do mundo");
+
+    Console.WriteLine ("Todos os dias");
+    Console.WriteLine ("Antes de dormir");
+    Console.WriteLine ("Lembro e esqueço");
+    Console.WriteLine ("Como foi o dia");
+    Console.WriteLine ("Sempre em frente");
+    Console.WriteLine ("Não temos tempo a perder");
+
+    Console.WriteLine ("Nosso suor sagrado");
+    Console.WriteLine ("É bem mais belo");
+    Console.WriteLine ("Que esse sangue amargo");
+    Console.WriteLine ("E tão sério");
+    Console.WriteLine ("E selvagem! Selvagem!");
+    Console.WriteLine ("Selvagem!");
+
+    Console.WriteLine ("Veja o sol");
+    Console.WriteLine ("Dessa manhã tão cinza");
+    Console.WriteLine ("A tempestade que chega");
+    Console.WriteLine ("É da cor dos teus olhos");
+    Console.WriteLine ("Castanhos");
+
+    Console.WriteLine ("Então me abraça forte");
+    Console.WriteLine ("E diz mais uma vez");
+    Console.WriteLine ("Que já estamos");
+    Console.WriteLine ("Distantes de tudo");
+    Console.WriteLine ("Temos nosso próprio tempo");
+    Console.WriteLine ("Temos nosso próprio tempo");
+    Console.WriteLine ("Temos nosso próprio tempo");
+
+    Console.WriteLine ("Não tenho medo do escuro");
+    Console.WriteLine ("Mas deixe as luzes");
+    Console.WriteLine ("Acesas agora");
+    Console.WriteLine ("O que foi escondido");
+    Console.WriteLine ("É o que se escondeu");
+    Console.WriteLine ("E o que foi prometido");
+    Console.WriteLine ("Ninguém prometeu");
+    Console.WriteLine ("Nem foi tempo perdido");
+    Console.WriteLine ("Somos tão jovens");
+
+    Console.WriteLine ("Tão jovens! Tão jovens!");
